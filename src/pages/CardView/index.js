612e80127dd39d2e0b625bf37673e1f526eae4f8
Zulaika <zulaika.ali@outlook.com>
2022-10-28 12:16:10 +0100

    add function to change questions

--- src/pages/CardView/index.js
+++ src/pages/CardView/index.js
@@ -1,17 +1,35 @@
-import React from 'react'
+import React, { useState } from 'react'
 import './CardView.css'
 import { Card } from '../../components'
+import { CaretLeftFill, CaretRightFill } from 'react-bootstrap-icons';
 
 const CardView = () => {
-    
+    const [cardId, setCardId] = useState(0)
+    const flashcards = [
+        { id: 1, question: "question 1", answer: "answer 1" },
+        { id: 2, question: "question 2", answer: "answer 2" },
+        { id: 3, question: "question 3", answer: "answer 3" },
+        { id: 4, question: "question 4", answer: "answer 4" }
+    ]
+    const nextQuestion = () => {
+        setCardId(cardId => cardId + 1)
+    };
+    const previousQuestion = () => {
+        setCardId(cardId => cardId - 1)
+    };
 
     return (
         <>
-            <div className="Cards">
-                <Card />
+        <div className="Cards">
+            <CaretLeftFill id="arrow" onClick={previousQuestion} />
+            <div>
+                <Card front={flashcards[cardId].question} back={flashcards[cardId].answer} />
+            </div>
+            <CaretRightFill id="arrow" onClick={nextQuestion} />
             </div>
         </>
     )
+
 };
 
 export default CardView

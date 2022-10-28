e95e9dde25fbb8849c3de02a8f8b7c44e1a14daa
Leehamb99 <leehamb99@gmail.com>
2022-10-28 10:34:34 +0100

    fix routes

--- src/App.js
+++ src/App.js
@@ -1,12 +1,13 @@
 import React from 'react'
 import { Routes, Route } from 'react-router-dom'
-import { CardView} from './pages';
+import { CardView, Dashboard } from './pages';
 
 function App() {
   return (
     <div className="App">
       <Routes>
-      <Route path="view" element={<CardView />} />
+      <Route path="dashboard" element={<Dashboard />}></Route>
+      <Route path="view" element={<CardView />}></Route>
       </Routes>
     </div>
   );

e64be4ee24bab3b2b706c8e6b3a540970c51954a
Camila Pessoa <caape13@hotmail.com>
2022-10-31 11:15:21 +0000

    move Header&Footer to layout folder, add Form component

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,3 +1,4 @@
 export { default as CardView } from './CardView'
 export { default as Dashboard } from './Dashboard'
 export { default as Home } from './Home'
+export { default as Registration } from './Registration'

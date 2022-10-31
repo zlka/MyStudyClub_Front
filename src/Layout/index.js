b6a039e94beecc979f0a171e65e1343ed40b2f9c
Camila Pessoa <caape13@hotmail.com>
2022-10-31 11:23:16 +0000

    Move header to Layout

--- src/Layout/index.js
+++ src/Layout/index.js
@@ -1,2 +1,14 @@
-export { default as Header } from './Header'
-export { default as Footer } from './Footer'
+import React from 'react';
+import { Header } from './Header';
+import { Footer } from './Footer';
+import { Outlet } from 'react-router-dom';
+
+export const Layout = () => (
+  <>
+    <Header token={removeToken} />
+    <main>
+      <Outlet />
+    </main>
+    <Footer />
+  </>
+)

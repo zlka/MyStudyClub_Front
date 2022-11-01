506456a91b88d8f28a46e61b376c5d044a5ac4e5
Zulaika <zulaika.ali@outlook.com>
2022-11-01 17:40:20 +0000

    add searchbar

--- src/Layout/Header/index.js
+++ src/Layout/Header/index.js
@@ -1,30 +1,16 @@
 import React,{ useState } from 'react'
-import axios from "axios"
+// import axios from "axios"
 import logo from '../../static/logo.png'
 import './header.css'
-import { XLg } from 'react-bootstrap-icons';
+import { XLg,Search } from 'react-bootstrap-icons';
 import { SignIn, SignUp } from '../../components/'
 
 function Header(props) {
   const [dis , setDisplay] = useState("")
   const [hidden , setHidden] = useState(true)
+  const [appear , setAppear] = useState(true)
 
-  // function logMeOut() {
-  //   axios({
-  //     method: "POST",
-  //     url: "http://127.0.0.1:5000/logout"
-  //   })
-
-  //   .then((response) => {
-  //     props.token()
-  //   }).catch((error) => {
-  //     if (error.response) {
-  //       console.warn(error.response)
-  //       console.warn(error.response.status)
-  //       console.warn(error.response.headers)
-  //     }
-  //   })
-  // }
+  
 
   const openModal= () => {
     setDisplay("block")
@@ -36,12 +22,17 @@ function Header(props) {
     setDisplay("none")
   };
 
-
   return(
     <header className="navHeader">
       <img src={logo} alt="my study club" />
 
-      <div className="registration">
+      <div className="searchBar" hidden={appear}>
+        <input className ="search" type="text" />
+        <button className="searchBtn" style={{color:'grey'}}><Search /></button>
+        
+      </div>
+
+      <div className="registration" >
         <button id="login" onClick={openModal} > Login </button> | 
         <button id="register" onClick={openModal}> Sign Up </button>
         {/* <button  onClick={logMeOut}>Logout </button> */}
@@ -67,3 +58,19 @@ function Header(props) {
 }
 
 export default Header
+// function logMeOut() {
+  //   axios({
+  //     method: "POST",
+  //     url: "http://127.0.0.1:5000/logout"
+  //   })
+
+  //   .then((response) => {
+  //     props.token()
+  //   }).catch((error) => {
+  //     if (error.response) {
+  //       console.warn(error.response)
+  //       console.warn(error.response.status)
+  //       console.warn(error.response.headers)
+  //     }
+  //   })
+  // }

f87d71a8fb79f46b31792717fd61b26047e0c17e
Zulaika <zulaika.ali@outlook.com>
2022-11-01 12:37:37 +0000

    add styling to login form

--- src/Layout/Header/index.js
+++ src/Layout/Header/index.js
@@ -9,22 +9,22 @@ function Header(props) {
   const [dis , setDisplay] = useState("")
   const [hidden , setHidden] = useState(true)
 
-  function logMeOut() {
-    axios({
-      method: "POST",
-      url: "http://127.0.0.1:5000/logout"
-    })
+  // function logMeOut() {
+  //   axios({
+  //     method: "POST",
+  //     url: "http://127.0.0.1:5000/logout"
+  //   })
 
-    .then((response) => {
-      props.token()
-    }).catch((error) => {
-      if (error.response) {
-        console.warn(error.response)
-        console.warn(error.response.status)
-        console.warn(error.response.headers)
-      }
-    })
-  }
+  //   .then((response) => {
+  //     props.token()
+  //   }).catch((error) => {
+  //     if (error.response) {
+  //       console.warn(error.response)
+  //       console.warn(error.response.status)
+  //       console.warn(error.response.headers)
+  //     }
+  //   })
+  // }
 
   const openModal= () => {
     setDisplay("block")
@@ -46,17 +46,18 @@ function Header(props) {
         <button id="register" onClick={openModal}> Sign Up </button>
         {/* <button  onClick={logMeOut}>Logout </button> */}
 
-        <div className="modal" style={{display: dis}} hidden={hidden}>
+        <div className="modal" style={{display: dis}} hidden={!hidden}>
           <div className="login-modal">
-            <SignIn />
-            <XLg onClick={closeModal} className="exit-btn"/>
+          <XLg onClick={closeModal} className="exit-btn"/>
+          <SignUp />
           </div>
         </div>
 
-        <div className="modal" style={{display: dis}} hidden={!hidden}>
+        <div className="modal" style={{display: dis}} hidden={hidden}>
           <div className="register-modal">
-            <SignUp />
+            
             <XLg onClick={closeModal} className="exit-btn"/>
+            <SignIn />
           </div>
         </div>
 

629532b726f5d2fee648406c42ab7e9e90ebab72
karl dudley <karldudley@gmail.com>
2022-11-02 14:48:26 +0000

    test

--- src/pages/Set/index.js
+++ src/pages/Set/index.js
@@ -1,14 +1,16 @@
 import { useState, useEffect } from 'react'
 import axios from "axios";
+import { useLocation } from "react-router-dom";
 import { FlashcardList } from '../../components';
 import './set.css'
 
 
 function Set() {
+  const location = useLocation()
     
   // new line start
   const [flashcards, setFlashcards] = useState(null)
-  const [setId, setSetId] = useState(1)
+  const [setId, setSetId] = useState(3)
 
   useEffect(() => {
     axios({

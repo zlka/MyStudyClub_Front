798ae70ff713aa3202814c0f5e41f0d270f51f39
Zulaika <zulaika.ali@outlook.com>
2022-11-03 12:19:05 +0000

    add create new set axios post

--- src/pages/ClubView/index.js
+++ src/pages/ClubView/index.js
@@ -1,14 +1,16 @@
 import React from 'react'
 import axios from 'axios';
-import {useEffect, useState } from 'react';
+import { useEffect, useState } from 'react';
 import { ClubCard } from '../../components'
 import './index.css'
-import { BackButton } from "../../components"
+import { BackButton, NewSet } from "../../components"
+import { PlusCircle } from 'react-bootstrap-icons'
 
 import { useLocation } from "react-router-dom";
 
 const ClubView = () => {
     const [sets, setSets] = useState(null)
+    const [hidden, setHidden] = useState(true)
     const location = useLocation()
 
     useEffect(() => {
@@ -17,30 +19,52 @@ const ClubView = () => {
         let config = {
             method: 'get',
             url: `https://my-study-club.herokuapp.com/sets/${location.state}`,
-            headers: { },
-            data : data
+            headers: {},
+            data: data
         };
 
         axios(config)
-        .then(function (response) {
-            console.log(JSON.stringify(response.data));
-            setSets(response.data)
-        })
-        .catch(function (error) {
-            console.log(error);
-        });
+            .then(function (response) {
+                console.log(JSON.stringify(response.data));
+                setSets(response.data)
+            })
+            .catch(function (error) {
+                console.log(error);
+            });
     }, [])
 
 
+    const addSet = () => {
+        setHidden(!hidden)
+
+    }
 
     return (
-            <div className="workouts">
-            {sets ? sets.map(set => (
-                    <ClubCard  name={set.set_name} key={set.id} set={set["club.club_name"]} />
+        <>             
+        <BackButton/>
+        
+        <div className="clubView">
+            <div className="clubs">
+                {sets ? sets.map(set => (
+                    <ClubCard name={set.set_name} key={set.id} set={set["club.club_name"]} />
                 )) : <h3> Loading </h3>}
             </div>
-        );   
-    }
- 
+            
+                <div className="newSet">
+                    <div onClick={addSet}>
+                        <button id="newSet" ><PlusCircle />Add a new set</button>
+                    </div>
+
+                    <div hidden={hidden}>
+                        <NewSet id={location.state}/>
+                    </div>
+
+                </div>
+            
+        </div>
+        </>
+    );
+}
+
 
 export default ClubView

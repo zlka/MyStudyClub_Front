4b7aeb107768995c5f571558256666f818ff7fdb
Leehamb99 <leehamb99@gmail.com>
2022-11-03 10:07:04 +0000

    fix merge

--- src/pages/ClubView/index.js
+++ src/pages/ClubView/index.js
@@ -3,15 +3,6 @@ import axios from 'axios';
 import {useEffect, useState } from 'react';
 import { ClubCard } from '../../components'
 import './index.css'
-<<<<<<< HEAD
-import { useLocation } from 'react-router-dom'
-
-const ClubView = () => {
-    const location = useLocation()
-    console.log(location.state)
-
-    return (
-=======
 import { BackButton } from "../../components"
 
 import { useLocation } from "react-router-dom";
@@ -19,7 +10,6 @@ import { useLocation } from "react-router-dom";
 const ClubView = () => {
     const [sets, setSets] = useState(null)
     const location = useLocation()
->>>>>>> 2f54b25b08c789b6ab4f4ca6e5dad9622356d017
 
     useEffect(() => {
         let data = '';
